146f21a5801ec51cb230d5836cf63c266fb9e9ef
Odat0620 <oda620@icloud.com>
2022-04-05 00:29:52 +0900

    コメントアウト取り消し

--- discordbot.py
+++ discordbot.py
@@ -15,20 +15,21 @@ client = commands.Bot(command_prefix=prefix)
 with open('emoji_ja.json', encoding='utf-8') as file:
     emoji_dataset = json.load(file)
 
-# @client.event
-# async def on_ready():
-#     presence = f'{prefix}ヘルプ | 0/{len(client.guilds)}サーバー'
-#     await client.change_presence(activity=discord.Game(name=presence))
+@client.event
+async def on_ready():
+    # presence = f'{prefix}ヘルプ | 0/{len(client.guilds)}サーバー'
+    presence = f''
+    await client.change_presence(activity=discord.Game(name=presence))
 
-# @client.event
-# async def on_guild_join(guild):
-#     presence = f'{prefix}ヘルプ | {len(client.voice_clients)}/{len(client.guilds)}サーバー'
-#     await client.change_presence(activity=discord.Game(name=presence))
+@client.event
+async def on_guild_join(guild):
+    presence = f'{prefix}ヘルプ | {len(client.voice_clients)}/{len(client.guilds)}サーバー'
+    await client.change_presence(activity=discord.Game(name=presence))
 
-# @client.event
-# async def on_guild_remove(guild):
-#     presence = f'{prefix}ヘルプ | {len(client.voice_clients)}/{len(client.guilds)}サーバー'
-#     await client.change_presence(activity=discord.Game(name=presence))
+@client.event
+async def on_guild_remove(guild):
+    presence = f'{prefix}ヘルプ | {len(client.voice_clients)}/{len(client.guilds)}サーバー'
+    await client.change_presence(activity=discord.Game(name=presence))
 
 @client.command()
 async def 接続(ctx):
@@ -132,8 +133,9 @@ async def on_message(message):
 async def on_voice_state_update(member, before, after):
     if before.channel is None:
         if member.id == client.user.id:
-            presence = f'{prefix}ヘルプ | {len(client.voice_clients)}/{len(client.guilds)}サーバー'
-            # await client.change_presence(activity=discord.Game(name=presence))
+            # presence = f'{prefix}ヘルプ | {len(client.voice_clients)}/{len(client.guilds)}サーバー'
+            presence = f'チャット読み上げるよ'
+            await client.change_presence(activity=discord.Game(name=presence))
         else:
             if member.guild.voice_client is None:
                 await asyncio.sleep(0.5)
@@ -147,8 +149,9 @@ async def on_voice_state_update(member, before, after):
                     member.guild.voice_client.play(discord.FFmpegPCMAudio(mp3url))
     elif after.channel is None:
         if member.id == client.user.id:
-            presence = f'{prefix}ヘルプ | {len(client.voice_clients)}/{len(client.guilds)}サーバー'
-            # await client.change_presence(activity=discord.Game(name=presence))
+            # presence = f'{prefix}ヘルプ | {len(client.voice_clients)}/{len(client.guilds)}サーバー'
+            presence = f'チャット読み上げるよ'
+            await client.change_presence(activity=discord.Game(name=presence))
         else:
             if member.guild.voice_client:
                 if member.guild.voice_client.channel is before.channel:

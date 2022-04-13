107657b20778d97cf66c5f7e6b799f8f6f8ffd97
Odat0620 <oda620@icloud.com>
2022-04-13 22:38:48 +0900

    [Change] redis_urlをheroku or ローカルで変わるよう変更
    ・デフォルトのプレフィックスを"🦑"から"$"へ変更

--- discordbot.py
+++ discordbot.py
@@ -8,7 +8,7 @@ import emoji
 import json
 import r
 
-prefix = os.getenv('DISCORD_BOT_PREFIX', default='🦑')
+prefix = os.getenv('DISCORD_BOT_PREFIX', default='$')
 token = os.environ['DISCORD_BOT_TOKEN']
 voicevox_key = os.environ['VOICEVOX_KEY']
 voicevox_speaker = os.getenv('VOICEVOX_SPEAKER', default='2')
--- r.py
+++ r.py
@@ -3,6 +3,9 @@ import os
 
 redis_url = os.environ['REDIS_URL']
 
+if not redis_url:
+    redis_url = "redis://db:6379"
+
 def connect():
     return redis.from_url(
         url=redis_url,
